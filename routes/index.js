2f23353a26f49e5f8d6aae2c8b02661e22ffd1fc
chashikajw <chashikajw007@gmail.com>
2018-05-11 23:52:12 +0530

    modified EER

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../connection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,6 +26,34 @@ router.get('/wie',function (req, res,next) {
 //get cs_chapter page
 router.get('/cs_chapter',function (req, res,next) {
     res.render('cs_chapter',{title:'cs_chaptern Details'});
+});
+
+/* GET event page. */
+router.get('/events', function(req, res, next) {
+
+    connection.query('SELECT * FROM article', function (err, rows) {
+        if (err){
+            throw err;
+        }else {
+            console.log(rows);
+            res.render('articles', {events:rows});
+        }
+    });
+
+});
+
+/* GET event page. */
+router.get('/event/:event_id', function(req, res, next) {
+
+    connection.query("SELECT * FROM article WHERE id = '"+req.params.event_id+ "'" , function (err, rows) {
+        if (err){
+            throw err;
+        }else {
+            console.log(rows);
+            res.render('event', {events:rows});
+        }
+    });
+
 });
 
 module.exports = router;
