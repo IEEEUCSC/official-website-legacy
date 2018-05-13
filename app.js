2790aa4eb9c250030afdd8dd8875ea2841629ce8
chashikajw <chashikajw007@gmail.com>
2018-05-13 20:47:45 +0530

    modify changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
