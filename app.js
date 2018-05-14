122d4287dba33e58e5d49a89b37482348c367019
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-05-14 19:39:01 +0530

    Change file structure

--- app.js
+++ app.js
@@ -11,21 +11,19 @@ var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 
 
-
-
 var app = express();
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,8 +31,8 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 app.use(function (req, res) {
@@ -44,14 +42,14 @@ app.use(function (req, res) {
 })
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
