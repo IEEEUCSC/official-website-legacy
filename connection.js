8a6ec47fd0f328ab611a514514bcf8ecf64d3c42
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-05-15 14:29:32 +0530

    Update connection

--- connection.js
+++ connection.js
@@ -2,10 +2,13 @@ var mysql = require('mysql');
 var db;
 
 var settings = {
-    host     : 'localhost',
-    user     : 'root',
-    password : '123',
+    // host     : 'localhost',
+    // user     : 'root',
+    // password : '123',
     // database : 'ieeedb',
+    host     : 'ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'jbom4gbnytqqnn6k',
+    password : 'wuq36r7691v2at52',
     database : 'b2gohrukcxgzmwwj',
     multipleStatements: true
 };
